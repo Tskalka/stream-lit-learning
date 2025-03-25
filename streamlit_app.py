4969568fd7c33ead71f6a0d7ef20d81c9816b7e5
Tyler Skalka <ska20004@byui.edu>
2025-03-25 15:51:58 -0600

    fixing code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,28 @@
 import streamlit as st
 import joblib
 import pandas as pd
-
-st.title("Gender Prediction App")
-
+st.title("Gender prediction")
 # Load the model
 @st.cache_resource
 def load_model():
     return joblib.load("naive_bayes_gender_model_US.pkl")
-
 model = load_model()
-
-st.subheader("Enter measurements to predict")
-
+# Input from user
+st.subheader("Enter mesuarements to predict")
 col1, col2 = st.columns(2)
-
 with col1:
-    feet = st.number_input("Height (feet)", min_value=1.0, max_value=7.0, value=5.0, step=1.0)
-
+    feet = st.number_input("Height (feet)", min_value=1 , max_value=8 , value=5, step=1)
 with col2:
-    inches = st.number_input("Inches", min_value=0.0, max_value=11.0, value=6.0, step=1.0)
-
-
+    inches = st.number_input("Height (inches)", min_value=0 , max_value=11 , value=6, step=1)
 height = feet + (12 /inches)
-    
-normalized_shoe_size = st.number_input("Assuming US Shoe Size", min_value=1.0, max_value=20.0, value=10.0, step=0.5)
-
+normalized_shoe_size = st.number_input("Shoe size US shoe", min_value=1.0, max_value=24.0, value=10.0, step=0.5)
 if st.button("Predict"):
-    input_data = pd.DataFrame({"height": [height], "shoe_size": [normalized_shoe_size]})
-
+    input_data = pd.DataFrame({
+        "height": [height],
+        "shoe_size": [normalized_shoe_size]
+    })
     prediction = model.predict(input_data)[0]
     probability = model.predict_proba(input_data)[0]
-
     st.subheader("Prediction result:")
-    st.write(f"Prediction: {prediction}")
-    st.write(f"Probability: {probability}")
+    st.write(f'Prediction: {prediction}')
+    st.write(f'Probability: {probability}')
