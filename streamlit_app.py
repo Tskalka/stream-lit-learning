642f57ca9423c6bd8d3b3192bb95a944d8ceac88
Tyler Skalka <ska20004@byui.edu>
2025-03-25 15:37:17 -0600

    fixing error

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ st.subheader("Enter measurements to predict")
 col1, col2 = st.columns(2)
 
 with col1:
-   height = st.number_input("Height (feet)", min_value=1.0, max_value=7.0, value=5.0, step=1)
+   height = st.number_input("Height (feet)", min_value=1.0, max_value=7.0, value=5.0, step=1.0)
 
 with col2:
     inches = st.number_input("Inches", min_value=0.0, max_value=11.0, value=6.0, step=1.0)
 
+    
