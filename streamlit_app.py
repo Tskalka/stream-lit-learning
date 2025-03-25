5a3691c4a07e67883d1e6dd30c9c0d06e4f02cc9
Tyler Skalka <ska20004@byui.edu>
2025-03-25 15:41:13 -0600

    adding shoe size

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,12 @@ st.subheader("Enter measurements to predict")
 col1, col2 = st.columns(2)
 
 with col1:
-   height = st.number_input("Height (feet)", min_value=1.0, max_value=7.0, value=5.0, step=1.0)
+    feet = st.number_input("Height (feet)", min_value=1.0, max_value=7.0, value=5.0, step=1.0)
 
 with col2:
     inches = st.number_input("Inches", min_value=0.0, max_value=11.0, value=6.0, step=1.0)
 
+
+height = feet + (12 /inches)
     
+normalized_shoe_size = st.number_input("Shoe Size", min_value=1.0, max_value=20.0, value=10.0, step=0.5)
